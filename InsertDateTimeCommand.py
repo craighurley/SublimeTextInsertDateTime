4d8249f3c0879b183fe7d609da2b3a88d10c5101
Craig Hurley <craig@thehurley.com>
2013-10-07 15:20:32 +1300

    Improved command naming

--- InsertDateTimeCommand.py
+++ InsertDateTimeCommand.py
@@ -3,8 +3,9 @@ import sublime_plugin
 import time
 
 format_datetime_iso     = "%Y%m%d%H%M%S"
-format_datetime_human   = "%H:%M:%S %d/%m/%Y"
-format_date_human       = "%d/%m/%Y"
+format_datetime         = "%d/%m/%Y %H:%M:%S"
+format_timedate         = "%H:%M:%S %d/%m/%Y"
+format_date             = "%d/%m/%Y"
 
 class InsertDateTimeIsoCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -19,12 +20,12 @@ class InsertDateTimeIsoCommand(sublime_plugin.TextCommand):
         sublime.set_clipboard(dt)
 
         # update the status message
-        sublime.status_message("Inserted ISO date time")
+        sublime.status_message("Inserted date time ISO")
 
-class InsertDateTimeHumanCommand(sublime_plugin.TextCommand):
+class InsertDateTimeCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # get the date time
-        dt = time.strftime(format_datetime_human);
+        dt = time.strftime(format_datetime);
 
         # paste the datetime at each selection point
         for cursors in self.view.sel():
@@ -34,12 +35,12 @@ class InsertDateTimeHumanCommand(sublime_plugin.TextCommand):
         sublime.set_clipboard(dt)
 
         # update the status message
-        sublime.status_message("Inserted human date time")
+        sublime.status_message("Inserted date time")
 
-class InsertDateHumanCommand(sublime_plugin.TextCommand):
+class InsertTimeDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # get the date time
-        dt = time.strftime(format_date_human);
+        dt = time.strftime(format_timedate);
 
         # paste the datetime at each selection point
         for cursors in self.view.sel():
@@ -49,4 +50,19 @@ class InsertDateHumanCommand(sublime_plugin.TextCommand):
         sublime.set_clipboard(dt)
 
         # update the status message
-        sublime.status_message("Inserted human date")
+        sublime.status_message("Inserted time date")
+
+class InsertDateCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # get the date time
+        dt = time.strftime(format_date);
+
+        # paste the datetime at each selection point
+        for cursors in self.view.sel():
+            self.view.replace(edit, cursors, dt)
+
+        # copy the last datetime to the clipboard
+        sublime.set_clipboard(dt)
+
+        # update the status message
+        sublime.status_message("Inserted date")
