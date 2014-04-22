d6fdad60f6a5170b4356ab74b9aa61a7bd4dace3
Craig Hurley <craig@thehurley.com>
2014-04-22 12:39:09 +1200

    Added a dropdown for picking the date format.

--- InsertDateTimeCommand.py
+++ InsertDateTimeCommand.py
@@ -2,67 +2,39 @@ import sublime
 import sublime_plugin
 import time
 
-format_datetime_iso     = "%Y%m%d%H%M%S"
-format_datetime         = "%d/%m/%Y %H:%M:%S"
-format_timedate         = "%H:%M:%S %d/%m/%Y"
 format_date             = "%d/%m/%Y"
-
-class InsertDateTimeIsoCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # get the date time
-        dt = time.strftime(format_datetime_iso);
-
-        # paste the datetime at each selection point
-        for cursors in self.view.sel():            
-            self.view.replace(edit, cursors, dt)
-
-        # copy the last datetime to the clipboard
-        sublime.set_clipboard(dt)
-
-        # update the status message
-        sublime.status_message("Inserted date time ISO")
-
-class InsertDateTimeCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # get the date time
-        dt = time.strftime(format_datetime);
-
-        # paste the datetime at each selection point
-        for cursors in self.view.sel():
-            self.view.replace(edit, cursors, dt)
-
-        # copy the last datetime to the clipboard
-        sublime.set_clipboard(dt)
-
-        # update the status message
-        sublime.status_message("Inserted date time")
-
-class InsertTimeDateCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # get the date time
-        dt = time.strftime(format_timedate);
-
-        # paste the datetime at each selection point
-        for cursors in self.view.sel():
-            self.view.replace(edit, cursors, dt)
-
-        # copy the last datetime to the clipboard
-        sublime.set_clipboard(dt)
-
-        # update the status message
-        sublime.status_message("Inserted time date")
+format_datetime         = "%d/%m/%Y %H:%M:%S"
+format_time             = "%H:%M:%S"
+format_timedate         = "%H:%M:%S %d/%m/%Y"
+format_datetime_xml     = "%Y-%m-%dT%H:%M:%S"
+format_datetime_iso     = "%Y%m%d%H%M%S"
 
 class InsertDateCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # get the date time
-        dt = time.strftime(format_date);
+    def on_done(self, index):
+        # if cancelled, index is returned as -1
+        if index == -1:
+            return
 
-        # paste the datetime at each selection point
+        # if user picks from list, return the correct entry
+        self.view.run_command("insert_text", {"args":{'text': self.list[index]}})
+
+    def run(self, edit):
+        self.list = [time.strftime(format_date), 
+                    time.strftime(format_datetime),
+                    time.strftime(format_time),
+                    time.strftime(format_timedate),
+                    time.strftime(format_datetime_xml),
+                    time.strftime(format_datetime_iso)]
+
+        self.view.window().show_quick_panel(self.list, self.on_done, 1, 0)
+
+class InsertTextCommand(sublime_plugin.TextCommand):
+    def run(self, edit, args):
         for cursors in self.view.sel():
-            self.view.replace(edit, cursors, dt)
+            self.view.replace(edit, cursors, args['text'])
 
         # copy the last datetime to the clipboard
-        sublime.set_clipboard(dt)
+        sublime.set_clipboard(args['text'])
 
         # update the status message
-        sublime.status_message("Inserted date")
+        sublime.status_message("Inserted text")
